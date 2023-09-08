b3e122b576f3957722b23b271240af08c1a88220
Harun <mdharunbd88@gmail.com>
2023-09-08 21:04:30 +0600

    base Modal

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import SiteNavigation from "@/components/SiteNavigation/SiteNavigation.vue";
 </script>
 
 <template>
-  <header>
-    home view
-  </header>
-
-  <RouterView />
+  <div class="flex flex-col min-h-screen bg-weather-primary font-Roboto">
+    <site-navigation />
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
